3a03118c6af9d6afb0e97c3d0b9c73b58fe642da
kevinalvarel <alvarelkevin12@gmail.com>
2025-08-24 22:44:00 +0700

    feat: Add headers configuration for Content Security Policy in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 // next.config.mjs (ESM)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
   allowedDevOrigins: ["snapper-concrete-really.ngrok-free.app"],
   experimental: {
     // other experimental flags
