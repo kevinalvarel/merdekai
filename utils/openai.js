ae1de1f35fb1c3758985355e527d039322576841
Muhammad Kevin Alvarel <alvarelkevin12@gmail.com>
2025-11-04 11:48:07 +0700

    Update openai.js

--- utils/openai.js
+++ utils/openai.js
@@ -3,7 +3,7 @@ const OpenAI = require("openai");
 require("dotenv").config();
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 async function openais(message) {
